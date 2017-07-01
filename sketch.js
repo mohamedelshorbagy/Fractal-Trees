31c0a635165eca5083004c0edfb156dbeee8f023
mohamedelshorbagy <mohamedelshorbagy96@gmail.com>
2017-07-01 05:13:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,16 +7,13 @@
 
  */ 
 
-
 var angle = 0;
-
 var slider;
 
 
 
 function setup() {
 createCanvas(400 , 400);
-
 slider = createSlider(0 , TWO_PI , 5.83 , 0.01);
 
 }
@@ -29,28 +26,43 @@ stroke(255);
 angle = slider.value();
 branch(100);
 
-}
+} // end of draw Function 
 
 
 
 function branch(len) {
+    // To Make Branches Every time has Different Colors  
+  if(len < 50) {
+    stroke(len * 20 /256 , 255 ,70);
+  } else {
+    
+    stroke(125 , 220 , 20);
+  }
+  
+  // Root Branch 
   line(0 , 0 , 0 , - len);
+  // Make End Point of Any Branch the Origin to Next 
   translate(0 , -len);
+  
+  
+  
   // Base Condition
   // Condition Which Stops the Recursion 
-
   if (len > 4) {
+  // Recursion Process
+  // To Right 
     push();
     rotate(angle);
     branch(len * 0.67);
     pop();
+  // To Left   
     push();
     rotate(-angle);
     branch(len * 0.67);
     pop();
 
-  }
+  } // end of Base Condition
 
 
 
-}
+} // End of Branch Function
